314bf03b3fdef59a022f6cd8ae194e3f2c4576cb
ryusuhan <ryusoohan@naver.com>
2018-09-16 21:20:18 +0900

    test5

--- routes/keyboard_option/main_keyboard.js
+++ routes/keyboard_option/main_keyboard.js
@@ -14,7 +14,7 @@ router.get('/', async(req,res) => {
 	}
 
 	// 전송하기
-	if(req == "처음으로"){
+	//if(req == "처음으로"){
 		res.status(200).send({
 			message : {
 				text : "서비스 선택!!\n\n"
@@ -23,12 +23,12 @@ router.get('/', async(req,res) => {
 				type: "buttons",
 				buttons: key
 			}
-		});
-	}else if(req == "keyboard"){
-		res.status(200).send({
-			type : "buttons",
-			buttons : key
-		});
+	//	});
+	//}else if(req == "keyboard"){
+	//	res.status(200).send({
+	//		type : "buttons",
+	//		buttons : key
+	//	});
 	}
 });
 
